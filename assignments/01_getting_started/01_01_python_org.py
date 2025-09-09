cb31bb96a16e0100a71f2acdfc05d22ad126cc5e
paloma-castro2401 <rapunzel.kratt@gmail.com>
2025-09-09 12:39:28 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,6 @@
+"""
+
+the python website features a lot of resources for anyone to access
+the section that interested me the most were the guides and resources that provide you with information on how to use python.
+
+"""
---
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,8 @@
+'''
+
+If I had infinite programming skills:
+I would use these skills to make my own video game like an rpg.
+I have a little bit of expirience using gamemaking engines like gamemaker and rpg maker
+so I would like to use my coding knowlegde to make my game more intereting.
+
+'''
