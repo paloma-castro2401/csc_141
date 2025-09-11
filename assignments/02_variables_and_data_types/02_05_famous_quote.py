f0d71b86c360a698fce59172bf23427fb9d8fee4
paloma-castro2401 <rapunzel.kratt@gmail.com>
2025-09-11 11:28:45 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_05_famous_quote.py
@@ -0,0 +1,13 @@
+#famous quote
+
+first_name = "Shigeru"
+last_name = "Miyamoto"
+
+full_name = f"{first_name}{last_name}"
+
+print (f"{first_name}","once said: A delayed game is eventually good, but a rushed game is forever bad")
+
+
+
+print (f"{first_name}   {full_name}")
+
---
+++ assignments/02_variables_and_data_types/02_06_famous_quote2.py
@@ -0,0 +1,6 @@
+#famous person quote
+
+famous_person = "Shigeru Miyamoto"
+message = "A delayed game is eventually good, but a rushed game is forever bad"
+
+print (f"{famous_person} once said, {message}")
